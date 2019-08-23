b934cbee85e14699fedffcb80c198109ec860661
BrianDoyle95 <briandoyle884@gmail.com>
2019-08-23 15:13:45 +0100

    menu scene complete

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -7,13 +8,50 @@ public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+    int currentResolutionIndex = 0;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+if(resolutions[i].width == Screen.currentResolution.width && 
+ resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
 
-    public  void SetQuality(int qualityIndex)
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen (bool isFullscreen)
+    {
+       Screen.fullScreen = isFullscreen;
     }
 }
